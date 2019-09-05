1b289926a8e3078e033bf1414f89951825af6a38
beehamer <ylxu@stanford.edu>
2019-09-05 14:33:27 -0700

    changed UI to compare the diff between s&p500 and the current strategy

--- render/StockTradingGraph.py
+++ render/StockTradingGraph.py
@@ -5,6 +5,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from matplotlib import style
+import pandas as pd
 
 # finance module is no longer part of matplotlib
 # see: https://github.com/matplotlib/mpl_finance
@@ -31,18 +32,31 @@ class StockTradingGraph:
     def __init__(self, df, title=None):
         self.df = df
         self.net_worths = np.zeros(len(df['Date']))
+        self.profit_values = np.zeros(len(df['Date']))
 
         # Create a figure on screen and set the title
         fig = plt.figure()
         fig.suptitle(title)
 
+        # Loading the s&p 500 data
+        self.sp_df = pd.read_csv('/Users/yilun/Desktop/File/work/bver/gym-env/Stock-Trading-Visualization/data/^GSPC.csv')
+        self.sp_df = self.sp_df[:len(df['Date'])]
+        # converting them to the actual profit
+        self.sp_array = self.sp_df['Close'].to_numpy()
+        self.initial_stock_num  = 10000.0 / self.sp_array[0]
+        self.sp_profit = np.zeros(len(self.sp_array))
+        for i in range(1, len(self.sp_array)):
+            self.sp_profit[i] = (self.sp_array[i] - self.sp_array[i - 1]) * self.initial_stock_num + self.sp_profit[i - 1]
+
+        # Add one more subplot for the profit
+        self.profit_ax = plt.subplot2grid((6, 1), (0, 0), rowspan= 2, colspan= 1)
         # Create top subplot for net worth axis
         self.net_worth_ax = plt.subplot2grid(
-            (6, 1), (0, 0), rowspan=2, colspan=1)
+            (6, 1), (2, 0), rowspan=2, colspan=1, sharex = self.profit_ax)
 
         # Create bottom subplot for shared price/volume axis
         self.price_ax = plt.subplot2grid(
-            (6, 1), (2, 0), rowspan=8, colspan=1, sharex=self.net_worth_ax)
+            (6, 1), (4, 0), rowspan=8, colspan=1, sharex=self.net_worth_ax)
 
         # Create a new axis for volume which shares its x-axis with price
         self.volume_ax = self.price_ax.twinx()
@@ -81,6 +95,46 @@ class StockTradingGraph:
         # Add space above and below min/max net worth
         self.net_worth_ax.set_ylim(
             min(self.net_worths[np.nonzero(self.net_worths)]) / 1.25, max(self.net_worths) * 1.25)
+
+
+    def _render_profit(self, current_step, net_worth, step_range, dates, starting_net_worth):
+        # Clear the frame rendered last step
+        self.profit_ax.clear()
+        profit = net_worth - starting_net_worth
+        if current_step == 0:
+            sp_profit = 0
+        else:
+            sp_profit = (self.sp_array[current_step] - self.sp_array[current_step - 1]) * self.initial_stock_num
+        self.profit_ax.plot_date(
+            dates, self.profit_values[step_range], '-', label='Profit', color = "red")
+        self.profit_ax.plot_date(
+            dates, self.sp_profit[step_range], '-', label='S&P 500 Profit', color = "blue")
+        # Show legend, which uses the label we defined for the plot above
+        self.profit_ax.legend()
+        legend = self.profit_ax.legend(loc=2, ncol=2, prop={'size': 8})
+        legend.get_frame().set_alpha(0.4)
+
+        last_date = date2num(self.df['Date'].values[current_step])
+        last_profit = self.profit_values[current_step]
+
+        # Annotate the current net worth on the net worth graph
+        self.profit_ax.annotate('{0:.2f}'.format(profit), (last_date, last_profit),
+                                   xytext=(last_date, last_profit),
+                                   bbox=dict(boxstyle='round',
+                                             fc='w', ec='k', lw=1),
+                                   color="black",
+                                   fontsize="small")
+        last_sp_profit = self.sp_profit[current_step]
+        self.profit_ax.annotate('{0:.2f}'.format(last_sp_profit), (last_date, last_sp_profit),
+                                   xytext=(last_date, last_sp_profit),
+                                   bbox=dict(boxstyle='round',
+                                             fc='w', ec='k', lw=1),
+                                   color="black",
+                                   fontsize="small")
+
+        # Add space above and below min/max net worth
+        self.net_worth_ax.set_ylim(
+            min(self.profit_values[np.nonzero(self.profit_values)]) / 1.25, max(self.profit_values) * 1.25)
 
     def _render_price(self, current_step, net_worth, dates, step_range):
         self.price_ax.clear()
@@ -156,6 +210,7 @@ class StockTradingGraph:
 
     def render(self, current_step, net_worth, trades, window_size=40):
         self.net_worths[current_step] = net_worth
+        self.profit_values[current_step] = net_worth - 10000 # TODO: need to pass in a parameter in place of the hardcoded number
 
         window_start = max(current_step - window_size, 0)
         step_range = range(window_start, current_step + 1)
@@ -163,6 +218,8 @@ class StockTradingGraph:
         # Format dates as timestamps, necessary for candlestick graph
         dates = np.array([date2num(x)
                           for x in self.df['Date'].values[step_range]])
+
+        self._render_profit(current_step, net_worth, step_range, dates, 10000)
 
         self._render_net_worth(current_step, net_worth, step_range, dates)
         self._render_price(current_step, net_worth, dates, step_range)
